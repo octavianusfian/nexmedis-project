b062d990c13a13aa0ca2a11af16ce689f2fa9f47
octavianusfian2 <pahoton28@gmail.com>
2025-02-21 00:14:52 +0700

    button bg color

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -22,16 +22,12 @@ const Button = ({
     <button
       style={{
         width: width,
+        backgroundColor: loading || disabled ? "[#E9E9E9]" : "black",
       }}
-      className={`rounded-2xl mb-3 bg-${
-        loading || disabled
-          ? "[#E9E9E9]"
-          : type === "primary"
-          ? "black"
-          : "[#E9E9E9]"
-      } text-white  py-2 px-5 font-semibold cursor-${
-        loading ? "not-allowed" : "pointer"
-      } transform hover:scale-105 transition-all duration-200 ease-in-out flex items-center justify-center gap-x-2 ${className}`}
+      className={`rounded-2xl mb-3 
+       text-white  py-2 px-5 font-semibold cursor-${
+         loading ? "not-allowed" : "pointer"
+       } transform hover:scale-105 transition-all duration-200 ease-in-out flex items-center justify-center gap-x-2 ${className}`}
       disabled={loading || disabled}
       onClick={to ? changeRoute : onClick}
       {...restProps}
