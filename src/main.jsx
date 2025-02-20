d2cbd11452372fab4a437e2b4eeae38dfed2f71d
octavianusfian2 <pahoton28@gmail.com>
2025-02-21 00:05:23 +0700

    typo App

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import App from "./app";
+import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
